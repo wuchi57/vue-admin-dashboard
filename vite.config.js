845eb210f293327796b92016d13b1a424d8d5653
wuchi <wuchi57@outlook.com>
2023-10-08 15:14:37 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      components: resolve(__dirname, 'src/components'),
+      api: resolve(__dirname, 'src/api'),
+      router: resolve(__dirname, 'src/router'),
+      store: resolve(__dirname, 'src/store'),
+      styles: resolve(__dirname, 'src/styles'),
+      views: resolve(__dirname, 'src/views'),
+      layouts: resolve(__dirname, 'src/layouts'),
+      utils: resolve(__dirname, 'src/utils'),
+      plugins: resolve(__dirname, 'src/plugins'),
+      dirs: resolve(__dirname, 'src/directives'),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  server: {
+    https: false,
+    port: 3000,
+    host: '0.0.0.0',
+    open: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ''), // stage
+      },
+    },
+  },
+  build: {
+    target: 'es2015',
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000,
+    reportCompressedSize: true,
+  },
 })
