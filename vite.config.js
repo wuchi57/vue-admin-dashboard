94e88c9fe9a5219353a325b1af845c70542da386
wuchi <wuchi57@outlook.com>
2023-10-08 15:42:40 +0800

    element-plus + icon

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
+/* eslint-enable */
 
 export default defineConfig({
   plugins: [
