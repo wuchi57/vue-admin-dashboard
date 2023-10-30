e897ec11341305f3c167b64aedc8b26cb53e97b5
wuchi <wuchi57@outlook.com>
2023-10-30 16:23:46 +0800

    feat: sidebar过渡效果

--- src/layout/components/main/index.vue
+++ src/layout/components/main/index.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="main">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade-transform" mode="out-in">
+        <component :is="Component" :key="key" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup>
-
+const route = useRoute()
+const key = computed(() => {
+  return route.path
+})
 </script>
 
 <style scoped lang="sass">
 .main
   padding: 20px
+
+/* fade-transform */
+.fade-transform-leave-active,
+.fade-transform-enter-active
+  transition: all .5s
+
+.fade-transform-enter
+  opacity: 0
+  transform: translateX(-30px)
+
+.fade-transform-leave-to
+  opacity: 0
+  transform: translateX(30px)
+
 </style>
